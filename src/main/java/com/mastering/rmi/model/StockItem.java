bb74e29913779a467a5c021ce1c7cf7bc5386858
alexandregama <alexandre.gama.lima@gmail.com>
2016-01-30 23:37:07 -0200

    Adds a StockItem and Book classes and removes unused classes

---
+++ src/main/java/com/mastering/rmi/model/StockItem.java
@@ -0,0 +1,35 @@
+package com.mastering.rmi.model;
+
+public class StockItem {
+
+	private Long id;
+
+	private Book book;
+
+	private int quantity;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Book getBook() {
+		return book;
+	}
+
+	public void setBook(Book book) {
+		this.book = book;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+}
