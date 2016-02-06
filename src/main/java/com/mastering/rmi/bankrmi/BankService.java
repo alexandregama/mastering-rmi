12f5da88b38dcee3bc5cc84a62b84459a66a6761
alexandregama <alexandre.gama.lima@gmail.com>
2016-02-06 11:05:40 -0200

    Adds a several methods that need to be valid as a Remote Method

--- src/main/java/com/mastering/rmi/bankrmi/BankService.java
+++ src/main/java/com/mastering/rmi/bankrmi/BankService.java
@@ -1,5 +1,6 @@
 package com.mastering.rmi.bankrmi;
 
+import java.io.IOException;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
@@ -14,8 +15,28 @@ public class BankService extends UnicastRemoteObject implements Bank {
 	}
 	
 	@Override
-	public Account getAccountFrom(String accountCode) throws RemoteException {
+	public Account getAccountFrom(String accountCode) throws RemoteException { //We don't need to write this Exception here
 		return dao.from(accountCode);
+	}
+
+	@Override
+	public void deposit(Account account, double value) throws Exception {
+		//Do something
+	}
+
+	@Override
+	public void credit(Account account, double value) throws IOException {
+		//Do something
+	}
+
+	@Override
+	public void approve() {
+		//Do something
+	}
+
+	@Override
+	public void disaprove() {
+		//Do something
 	}
 
 }
--- src/main/java/com/mastering/rmi/bankrmi/BankServiceRendpointRegister.java
+++ src/main/java/com/mastering/rmi/bankrmi/BankServiceRendpointRegister.java
@@ -12,4 +12,5 @@ public class BankServiceRendpointRegister {
 		Naming.rebind("/bankservice", new BankService());
 		System.out.println("BankService Endpoint is Active!");
 	}
+	
 }
